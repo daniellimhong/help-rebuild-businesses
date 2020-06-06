262416f80e5b885c0fd32c8fed5ab2f794d11745
Daniel Hong <daniellimhong@gmail.com>
2020-06-05 19:15:14 -0700

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { useFirebase } from "./firebase/FirebaseContext";
 
 function App() {
+  //Firebase connection testing
+  const [listings, setListings] = useState([]);
+  const firebase = useFirebase();
+
+  useEffect(() => {
+    //* get function for listings
+    firebase.listings().once("value", (snapshot) => {
+      const dbReponse = snapshot.val();
+      setListings(formatData(dbReponse));
+    });
+  }, []);
+
+  const formatData = (dataObj) => {
+    let formattedArr = [];
+    for (let listing in dataObj) {
+      const val = dataObj[listing];
+      val["listing"] = listing;
+      formattedArr.push(val);
+    }
+    return formattedArr;
+  };
+
+  const saveListing = (e) => {
+    //* This is the post function for listings to firebase db. this needs to be refactored,
+    e.preventDefault();
+
+    let listingData = {
+      title: "test listing 2",
+      business_name: "other business",
+      description: "desc2",
+      location: "LA",
+      platform: "gofundme",
+      isQualified: false,
+    };
+
+    firebase.listings().push(listingData, () => {
+      console.log("Listing Saved");
+    });
+  };
+
+  console.log(listings)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {listings.map((listing, index) => {
+        return (
+          <div key={`listing key ${index}`}>
+            <p>{listing.title}</p>
+            <p>{listing.business_name}</p>
+            <p>{listing.location}</p>
+            <p>{listing.platform}</p>
+          </div>
+        );
+      })}
+      <button onClick={saveListing}>save2</button>
     </div>
   );
 }
